d2f688190154e54c4f20310c0358b04085867dbe
sailking <wsh809@qq.com>
2018-07-31 16:55:17 +0200

    Update views.py
    todo:
    1. 从数据库读取数据生成表格
    2. 选择日期来获取某一时间段的数据
    3. 将新增加的数据更新到数据库中

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+# *-* coding=utf-8 *-*
+
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from pprint import pprint
+import json
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -14,7 +18,7 @@ def products():
 
 @app.route("/logistics")
 def logistics():
-    return render_template("logistics.html")
+    return render_template("logistics.html") 
 
 @app.route("/tables")
 def tables():
@@ -23,6 +27,15 @@ def tables():
 @app.route("/test")
 def test():
     return render_template("test.html")
+
+@app.route("/datafromtable", methods=['GET','POST'])
+def datafromtable():
+    if request.method == "POST":
+        print("method is post")
+        data = request.get_data()   #从post请求中获得原始json数据
+        dict = json.loads(data)[0]    #将json数据转换成python数据格式
+        print(dict)
+    return "success"
 
 if __name__ == "__main__":
     app.run(debug=True)
