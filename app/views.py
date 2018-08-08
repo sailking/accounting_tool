69771a7e0c093a14a3c4dddf19f5561338ec2a6d
sailking <wsh809@qq.com>
2018-08-08 16:55:21 +0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -16,23 +16,33 @@ def index():
 @app.route("/get_batch_list", methods=["POST"])
 def get_batch_lset():
     original_data = request.get_data()
-    return Purchase.get_batch_list()
+    return Purchase.get_batch_list(original_data.decode())
 
 @app.route("/get_purchase", methods=["POST"])
 def get_purchase():
     original_data = request.get_data()
-    return Purchase.get_purchase(original_data.decode())
+    return Purchase.get_purchase(json.loads(original_data))
 
 @app.route("/purchase_save", methods=["POST"])
 def purchase_save():
     original_data = request.get_data()
     data = json.loads(original_data)
     return Purchase.seed(data)
+
 @app.route("/purchase_delete", methods=["POST"])
 def purchase_delete():
     original_data = request.get_data()
-    data = json.loads(original_data)
-    return Purchase.purchase_delete(data)
+    return Purchase.purchase_delete(json.loads(original_data))
+
+@app.route("/get_product_names", methods=["POST"])
+def get_product_names():
+    original_data = request.get_data()
+    return Products.get_product_names(original_data.decode())
+
+@app.route("/get_product", methods=["POST"])
+def get_product():
+    original_data = request.get_data()
+    return Products.get_product(original_data.decode())
 
 @app.route("/products")
 def products():
@@ -42,7 +52,7 @@ def products():
 def get_type_list():
     return Products.get_product_type_list()
 
-@app.route("/get_product", methods=["POST"])
+@app.route("/get_product_list", methods=["POST"])
 def get_product_list():
     original_data = request.get_data()
     return Products.get_product_list(original_data.decode())
@@ -70,8 +80,6 @@ def tables():
 @app.route("/base")
 def test():
     return render_template("base.html")
-
-
 
 if __name__ == "__main__":
     connect_database()
